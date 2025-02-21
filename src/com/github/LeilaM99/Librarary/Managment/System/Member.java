28ad8c27295d5336f6dc9882caa255504bc088aa
Leila Mansouri <l71mansouri@gmail.com>
2025-02-21 11:35:30 +0330

    push member class

--- src/com/github/LeilaM99/Librarary/Managment/System/Member.java
+++ src/com/github/LeilaM99/Librarary/Managment/System/Member.java
@@ -1,4 +1,15 @@
 package com.github.LeilaM99.Librarary.Managment.System;
 
 public class Member {
+    private String name;
+    private String memberId;
+
+    public Member(String name, String memberId) {
+        this.name = name;
+        this.memberId = memberId;
+    }
+
+
+    public String getName() { return name; }
+    public String getMemberId() { return memberId; }
 }
